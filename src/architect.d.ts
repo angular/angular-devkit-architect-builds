cd06743da5a14cbb89443438794cd53a72665ef9
Angular Builds <circleci@angular.io>
2019-05-07 19:03:00 +0000

    520b449 docs(@angular/cli): fix typo with service schema (#14306)

--- src/architect.d.ts
+++ src/architect.d.ts
@@ -15,11 +15,10 @@ export interface ScheduleOptions {
 }
 export declare class Architect {
     private _host;
-    private _registry;
     private readonly _scheduler;
     private readonly _jobCache;
     private readonly _infoCache;
-    constructor(_host: ArchitectHost, _registry?: json.schema.SchemaRegistry, additionalJobRegistry?: experimental.jobs.Registry);
+    constructor(_host: ArchitectHost, registry?: json.schema.SchemaRegistry, additionalJobRegistry?: experimental.jobs.Registry);
     has(name: experimental.jobs.JobName): Observable<boolean>;
     scheduleBuilder(name: string, options: json.JsonObject, scheduleOptions?: ScheduleOptions): Promise<BuilderRun>;
     scheduleTarget(target: Target, overrides?: json.JsonObject, scheduleOptions?: ScheduleOptions): Promise<BuilderRun>;
