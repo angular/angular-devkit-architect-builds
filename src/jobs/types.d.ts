db1b8cb6806e5ef79fa4c3b10c9d72fea5161dc3
Angular Builds <circleci@angular.io>
2023-01-13 13:26:00 +0000

    b0004817c build: update dev-infra packages

--- src/jobs/types.d.ts
+++ src/jobs/types.d.ts
@@ -5,11 +5,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export declare type DeepReadonly<T> = T extends (infer R)[] ? DeepReadonlyArray<R> : T extends Function ? T : T extends object ? DeepReadonlyObject<T> : T;
-export declare type DeepReadonlyArray<T> = Array<DeepReadonly<T>>;
-export declare type DeepReadonlyObject<T> = {
+export type DeepReadonly<T> = T extends (infer R)[] ? DeepReadonlyArray<R> : T extends Function ? T : T extends object ? DeepReadonlyObject<T> : T;
+export type DeepReadonlyArray<T> = Array<DeepReadonly<T>>;
+export type DeepReadonlyObject<T> = {
     readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
-export declare type Readwrite<T> = {
+export type Readwrite<T> = {
     -readonly [P in keyof T]: T[P];
 };
